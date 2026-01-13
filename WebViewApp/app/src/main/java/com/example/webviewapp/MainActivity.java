1e5f88d6db9c751ce248314a179e48d88820ee8e
Alex N. Jose <alexnj@google.com>
2026-01-13 14:10:44 -0800

    Initial commit

---
+++ WebViewApp/app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.webviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://www.google.com");
+    }
+}
